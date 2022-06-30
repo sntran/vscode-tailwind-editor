c59f7c904d71ac94e5dd09a12bf508ab2adc4a9f
Son Tran-Nguyen <github@sntran.com>
2022-06-30 11:54:59 -0500

    build: no need for server

--- scripts/server.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S deno run --allow-net --allow-read --watch
-import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.145.0/http/file_server.ts";
-
-serve((req: Request) => {
-  return serveDir(req, {
-    showDirListing: true,
-  });
-}, { port: 8888 });
